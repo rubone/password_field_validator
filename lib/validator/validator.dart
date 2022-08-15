14ac431bfa29cad01dddf6b2b71170627850689c
rubone <rubone02@gmail.com>
2022-08-15 17:46:35 -0600

    :construction: Add validator class

---
+++ lib/validator/validator.dart
@@ -0,0 +1,33 @@
+class Validator {
+  //Check  minLength
+  bool hasMinimunLength(String password, int minLength) {
+    return password.length >= minLength ? true : false;
+  }
+
+  //Checks uppercaseCount
+  bool hasMinimumUppercase(String password, int uppercaseCount) {
+    String pattern = '^(.*?[A-Z]){' + uppercaseCount.toString() + ',}';
+    return password.contains(RegExp(pattern));
+  }
+
+  //Check lowercaseCount
+  bool hasMinimumLowercase(String password, int lowercaseCount) {
+    String pattern = '^(.*?[a-z]){' + lowercaseCount.toString() + ',}';
+    return password.contains(RegExp(pattern));
+  }
+
+  //Check numericCount
+  bool hasMinimumNumericCharacters(String password, int numericCount) {
+    String pattern = '^(.*?[0-9]){' + numericCount.toString() + ',}';
+    return password.contains(RegExp(pattern));
+  }
+
+  //Checks specialCharactersCount special character
+  bool hasMinimumSpecialCharacters(
+      String password, int specialCharactersCount) {
+    String pattern = r"^(.*?[$&+,\:;/=?@#|'<>.^*()_%!-]){" +
+        specialCharactersCount.toString() +
+        ",}";
+    return password.contains(RegExp(pattern));
+  }
+}
