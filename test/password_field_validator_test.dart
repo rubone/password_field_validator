677a7b4c5592567d7a661ac6c3cd62a455a64281
rubone <rubone02@gmail.com>
2022-08-16 17:17:58 -0600

    :white_check_mark: Add test to validator function hasMinimumLength

--- test/password_field_validator_test.dart
+++ test/password_field_validator_test.dart
@@ -1,12 +1,38 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:password_field_validator/password_field_validator.dart';
+import 'package:password_field_validator/validator/validator.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  late Validator validator;
+
+  setUp(() {
+    validator = Validator();
+  });
+
+  test(
+      'Should return false when minimum length is 8 and password has 6 characters',
+      () {
+    // Arrange
+    String password = 'reto45';
+    int minLength = 8;
+
+    // Act
+    bool result = validator.hasMinimumLength(password, minLength);
+
+    // Assert
+    expect(result, false);
+  });
+
+  test(
+      'Should return true when minimum lenght is 8 and password has 9 characters',
+      () {
+    // Arrange
+    String password = 'aratho988';
+    int minLenght = 8;
+
+    // Act
+    bool result = validator.hasMinimumLength(password, minLenght);
+
+    // Assert
+    expect(result, true);
   });
 }
